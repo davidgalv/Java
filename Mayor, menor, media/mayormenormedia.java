96558e26b1a71bfae0d02e5f9a2f6fd82246d32f
David GF <davidgalv8@gmail.com>
2017-11-03 11:59:41 +0100

    mayor, menor, media

---
+++ Mayor, menor, media/mayormenormedia.java
@@ -0,0 +1,23 @@
+/*-Nombre: menormayormedia
+* -Hallar el menor, el mayor y la media de n números positivos leidos por teclado.
+* -Entrada: Números leidos por teclado.
+* -Salida: Menor, mayor y media.
+* -Requisitos: Los números tienen que ser positivos.
+* 
+* Pseudocódigo Generalizado:
+* Inicio
+* 	Leer y validar si quiere ejecutar
+* 	Mientras si quiera
+* 		Leer y validar contador
+* 		Mientras el contador no llegue a lo especificado
+* 			Pedir numeros
+* 			Actualizar VCB
+* 		Fin mientras
+* 		Si el contador llega a lo especificado
+* 			Hacer calculos
+* 			Mostrar resultados
+* 		Fin si
+* 	Leer y validar si quiere volver a ejecutar
+* 	Fin mientras
+* Fin
+*/
